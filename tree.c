364636ec07408bf05498f312276f73d56d790f63
aquiles <jjoseaquiless@gmail.com>
2018-09-22 18:03:08 -0300

    adicionar nó em arvore
    Signed-off-by: aquiles <jjoseaquiless@gmail.com>, manuengsf <manusenac@hotmail.com>

--- tree.c
+++ tree.c
@@ -19,36 +19,47 @@ node* add(int n2, node* noderaiz){
       int  n1= noderaiz->number;      
    if(n1>n2){
       noderaiz->right=node2;  
-      printf("%d",noderaiz->right->number);
-      printf("w");
+      printf("%d \n",noderaiz->right->number);
+      printf("w \n");
    }else{
       noderaiz->left=node2;  
-      printf("%d" ,noderaiz->left->number);
-      printf("a");  
+      printf("%d \n" ,noderaiz->left->number);
+      printf("a \n");  
    }
    return noderaiz;     
 }
 
         
-node* nodeadda(node* noadd, node* raiz){
-      int n1, n2;
-      node* raiz2;  
-      raiz2=raiz;
-      while(raiz2!=NULL){
-         n1=raiz2->number;    
-         n2=noadd->number;   
-         if(n1>n2){
-             raiz2=raiz2->left;
-             printf("e"); 
+node* nodeadda(node* noadd, int n){
+      int i=0;
+
+      while(noadd!=NULL){       
+         if(noadd->number>n){
+             noadd=noadd->left;
+             if(noadd == NULL){
+                noadd=(node*)malloc(sizeof(node));
+                noadd->left=NULL;
+                noadd->right=NULL;
+                noadd->number = n;  
+                printf("e \n"); 
+                break;
+             }
          }else{
-            if(n1<n2){
-             raiz2=raiz2->right; 
-             printf("o");             
+             noadd=noadd->right;
+             if(noadd == NULL){
+                noadd=(node*)malloc(sizeof(node));
+                noadd->left=NULL;
+                noadd->right=NULL;
+                noadd->number = n;  
+    
+                printf("o \n");
+                break;
+             }             
             }
          }
-      }     
-      raiz2=noadd; 
-      return raiz2;
+           
+      
+      return noadd;
     
 }            
                   
@@ -69,7 +80,7 @@ arvore* crianoinicial(node* noinicial, arvore* arvore1, int n){
 arvore* countdireita(node* contado, int n){
     int i3=0;
     int i=0;
-    arvore* arvore1;
+    arvore * arvore1;
     arvore1=(arvore*) malloc(sizeof(arvore));  
     crianoinicial(contado,arvore1,n);       
     while(i3==0){
@@ -151,34 +162,50 @@ int converte(node* raiz){
      return 0;
     } 
 }         
-node* busca(int n2, node* raiz){
+void busca(int n2, node* raiz){
+      printf("passo 1 \n");
       int n1;
+      printf("passo 2 \n");
       int bool2=0;
-      bool2=converte(raiz);  
-      n1=raiz->number;         
-      while(bool2==0){    
+      printf("passo 3 \n");
+      bool2=converte(raiz);
+      printf("passo 4 \n");
+      n1=raiz->number;
+      printf("passo 5 \n");         
+      while(bool2==0){
+          printf("passo 6 \n");    
          if(n1>n2){  
-              raiz=raiz->right;   
+             printf("passo 7 \n");
+              raiz=raiz->right;
+              printf("passo 8 \n");  
          }else{
+             printf("passo 7,5 \n");
             if(n1<n2){
-              raiz=raiz->left;              
+                printf("passo 8.5 \n");
+              raiz=raiz->left;
+              printf("passo 9.5 \n");              
             }
          }
          if(raiz==NULL||n1==n2){
+             printf("passo 10 \n");
            bool2=1;
+           printf("passo 10.5 \n");
          }else{
+             printf("passo 11 \n");
            bool2=0;
-           n1=raiz->number;    
+           printf("passo 12 \n");
+           n1=raiz->number;
+           printf("passo 13 \n");    
          }         
                  
       } 
-      if(n2==raiz->number){
+      if(n2==n1){
         printf("o numero foi encontrado");
       }else{
         printf("o numero nao foi encontrado");
       }           
-      return raiz;   
- }        
+      //return raiz;   
+}        
                                   
 int tamanhohierarquia(node* pai, int  n){
     int n1, i;
@@ -193,7 +220,7 @@ int tamanhohierarquia(node* pai, int  n){
     abreesquerda(arvore1,arvoreg,n);
     
 }  
-arvore desenharvore(node* raiz){
+arvore desenharvore(node* raiz, node* no){
     char resposta;
     resposta='s';   
     int n;
@@ -201,16 +228,19 @@ arvore desenharvore(node* raiz){
     noadd=(node*)malloc(sizeof(node)); 
     noadd->left=NULL;
     noadd->right=NULL;  
-    int i;     
+    int i = 0;     
     while(resposta=='s'){
-        printf("digite o valor a ser colocado na arvore");
+        printf("digite o valor a ser colocado na arvore \n");
         scanf("%d",&n); 
         flush_in();
-        printf("voce deseja criar um novo no digite s para sim ou n para nao");
+        printf("voce deseja criar um novo no digite s para sim ou n para nao \n");
         scanf("%c",&resposta);
         flush_in();
-        noadd->number=n;
-        nodeadda(noadd,raiz);    
+        if(i == 0){
+            noadd->number=n;
+            raiz->number=n;
+        }
+        no = nodeadda(noadd,n);    
         i++; 
     } 
     arvore arvore1; 
@@ -221,18 +251,18 @@ arvore desenharvore(node* raiz){
 } 
  
 int main(){
-   node* raiz;
+   node* raiz, *no;
    raiz=(node*)malloc(sizeof(node));
    raiz->left=NULL;
    raiz->right=NULL; 
    arvore arvore1;        
    int n;   
-   arvore1=desenharvore(raiz);
+   arvore1=desenharvore(raiz,no);
    
    n=arvore1.cont; 
    printf("%d",countdireita(arvore1.pai[0], n)->cont);  
    printf("digite um valor para ser buscado");
    scanf("%d",&n); 
-   busca(n,raiz); 
+   busca(n,no); 
    return 0;   
 }
